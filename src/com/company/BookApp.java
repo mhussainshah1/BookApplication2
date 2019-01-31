8f5fb50d638bc4c9510e636e3fb6efa042d949f6
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-01-30 23:16:23 -0500

    Initial Commit

--- src/com/company/BookApp.java
+++ src/com/company/BookApp.java
@@ -1,18 +1,18 @@
 package com.company;
 /**
  * Create a project in IntelliJ called BookApplication.
- *
+ * <p>
  * Create a Book class that allows for Book title, author, description, price and isInStock variables
- *
+ * <p>
  * The Book class shall contain private member variables, a default constructor, an overloaded constructor and a method
  * called getDisplayText() which prints the author, title and description.
- *
- *
+ * <p>
+ * <p>
  * The application shall also contain getters and setters for each private member variable.
- *
+ * <p>
  * Create another class called BookApp which contains the main method. In the main method you shall create an instance
  * of a Book and print the Author, Title and Description.
- *
+ * <p>
  * Publish your solution to GitHub and submit your GitHub url as the assignment.
  */
 
@@ -27,16 +27,16 @@ import java.util.List;
 public class BookApp {
     public static void main(String[] args) {
         List<Book> books = new ArrayList<>();
-        books.add(new Book("OCA","Jeanne Boyarsky","OCAJP Exam guide",20, true));
-        books.add(new Book("OCP","Jeanne Boyarsky","OCPJP Exam guide",20, true));
-        books.add(new Book("OCA & OCP","Mala Gupta","OCA OCP JP Exam guide",20, false));
-        books.add(new Book("Java 5","Ivor Hornton","Basic Java",20, true));
-        books.add(new Book("Oracle Tutorial","Oracle Press","OCA OCP JP Exam guide",20, true));
-        books.add(new Book("Head First in Java","Jeanne Boyarsky","Servlet & JSP",20, false));
-        books.add(new Book("Java For Dummies","John","Basic Java",20, true));
+        books.add(new Book("OCA", "Jeanne Boyarsky", "OCAJP Exam guide", 20, true));
+        books.add(new Book("OCP", "Jeanne Boyarsky", "OCPJP Exam guide", 20, true));
+        books.add(new Book("OCA & OCP", "Mala Gupta", "OCA OCP JP Exam guide", 20, false));
+        books.add(new Book("Java 5", "Ivor Hornton", "Basic Java", 20, true));
+        books.add(new Book("Oracle Tutorial", "Oracle Press", "OCA OCP JP Exam guide", 20, true));
+        books.add(new Book("Head First in Java", "Jeanne Boyarsky", "Servlet & JSP", 20, false));
+        books.add(new Book("Java For Dummies", "John", "Basic Java", 20, true));
 
         BookApp app = new BookApp();
-        System.out.println("The total is: " +app.getTotal(books));
+        System.out.println("The total is: " + app.getTotal(books));
     }
 
     public double getTotal(List<Book> books) {
